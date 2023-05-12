07e6d698af603a6b8aef5ac86afde788c46b6c5c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-12 11:32:08 +0600

    Complete navbar with brand name

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header.</h2>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home" className='fs-3 fw-semibold'>YummyFood</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Link className='text-decoration-none text-light mx-2 fs-5' to="/">Home</Link>
+                        <Link className='text-decoration-none text-light mx-2 fs-5' to="/blogs">Blogs</Link>
+                    </Nav>
+                    <Nav>
+                        <Link>
+                            <Button variant="light" className='btn-lg fs-5'>Login</Button>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
